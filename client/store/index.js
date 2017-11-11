883626e5403621bbf6911c090b6754a4d97ce4cc
Phong Huynh <xphong@gmail.com>
2017-11-11 09:53:10 -0500

    Update store

--- client/store/index.js
+++ client/store/index.js
@@ -1,39 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 const state = {
-  count: 0
+  data: []
 }
 
 const mutations = {
-  INCREMENT (state) {
-    state.count++
-  },
-  DECREMENT (state) {
-    state.count--
+  RECEIVE_CHARACTERS (state, { characters }) {
+    state.data = characters
   }
 }
 
 const actions = {
-  incrementAsync ({ commit }) {
-    setTimeout(() => {
-      commit('INCREMENT')
-    }, 200)
-  },
-  increment ({ commit }) {
-    commit('INCREMENT')
-  },
-  decrement ({ commit }) {
-    commit('DECREMENT')
+  async fetchCharacters ({ commit }, name) {
+    const url = `http://localhost:8080/api/characters?limit=12&name=${name}`
+    const { data } = await axios.get(url)
+    commit('RECEIVE_CHARACTERS', { characters: data.results })
+  }
+}
+
+const getters = {
+  characters: state => {
+    return state.data.map(data => {
+      return {
+        name: data.name,
+        url: data.urls[1] ? data.urls[1].url : data.urls[0].url,
+        image: `${data.thumbnail.path}.${data.thumbnail.extension}`,
+        description: data.description === '' ? 'No description listed for this character.' : data.description
+      }
+    })
   }
 }
 
 const store = new Vuex.Store({
   state,
   mutations,
-  actions
+  actions,
+  getters
 })
 
 export default store
