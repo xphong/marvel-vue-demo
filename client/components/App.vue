dc02236c2f4f17139b9399330720f2b6ee4007d2
Phong Huynh <xphong@gmail.com>
2017-11-11 10:58:39 -0500

    Update home page styles

--- client/components/App.vue
+++ client/components/App.vue
@@ -10,12 +10,55 @@
 body {
   background-color: #f1f2f0;
   color: #35342f;
-  display: flex;
-  justify-content: center;
   font-family: 'Lato', sans-serif;
 }
 
 .page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   margin: 80px auto;
+}
+
+.fadeIn-animation {
+  animation: fadeIn 1.5s;
+}
+
+.fadeIn-page-animation {
+  animation: fadeIn 1s;
+}
+
+@keyframes fadeIn {
+    from {
+        opacity:0;
+    }
+    to {
+        opacity:1;
+    }
+}
+@-moz-keyframes fadeIn {
+    from {
+        opacity:0;
+    }
+    to {
+        opacity:1;
+    }
+}
+@-webkit-keyframes fadeIn {
+    from {
+        opacity:0;
+    }
+    to {
+        opacity:1;
+    }
+}
+@-o-keyframes fadeIn {
+    from {
+        opacity:0;
+    }
+    to {
+        opacity: 1;
+    }
 }
 </style>
